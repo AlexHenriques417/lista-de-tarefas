9c1bbff98657a0549ab3a08014e472a59722786f
Alex-031 <alexhenriques417@gmail.com>
2026-02-17 10:19:03 -0300

    Tela do Front em Desenvolvimento

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,29 @@
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Separator } from '@/components/ui/separator'
+import { Plus, List, CircleMinus, Check } from 'lucide-react';
+
 const Home = () => {
   return (
-    <div>Alex</div>
+    <main className ="w-full h-screen bg-gray-100 flex justify-center items-center">
+
+      <Card className="w-lg p-4">
+        <div className="flex gap-2">
+          <Input placeholder="Adicionar tarefa..." />
+          <Button variant="default" className="cursor-pointer"><Plus />Cadastrar</Button>
+        </div>
+
+        <Separator />
+
+        <div className="flex gap-2">
+          <Badge className="cursor-pointer"><List /> Todas</Badge>
+          <Badge className="cursor-pointer"><CircleMinus /> Não finalizadas</Badge>
+          <Badge className="cursor-pointer"><Check /> Concluídas</Badge>
+        </div>
+      </Card>
+    </main>
   )
 }
 
